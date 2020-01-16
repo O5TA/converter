760516bde921c34dabdb8bc173bc71b9845fd8e2
O5TA <serfer.od@gmail.com>
2020-01-16 15:46:22 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<link rel='stylesheet' id='main-style-css' href='style.css' type='text/css'/>
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="script.js"></script>
+<body>
+<div class="form_convert">
+    <form method="post">
+        <input type="text" id="get" name="amount" placeholder="Меняю"><span class="currency">USD</span>
+        <input type="text" id="take" name="take" placeholder="Получаю"><span class="currency">CNY</span>
+        <p><input type='submit' name="submit" value="Рассчитать"/></p>
+    </form>
+</div>
+</body>
+</html>
+
